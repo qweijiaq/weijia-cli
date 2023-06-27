15f4368f19d28e5e54b5eba0d88a8671097e5a40
WeiJia <2112025700@qq.com>
2023-06-27 21:44:13 +0800

    fix bug

--- core/core/bin/index.js
+++
@@ -1,9 +0,0 @@
-#! /user/bin/env node
-
-const importLocal = require("import-local");
-
-if (importLocal(__filename)) {
-  require("npmlog").info("cli", "正在使用 weijia-cli 本地版本");
-} else {
-  require("../lib")(process.argv.slice(2));
-}
